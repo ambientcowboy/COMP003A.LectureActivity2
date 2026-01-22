da9f232d8ef7948ce72a21518402154042a9e31c
ambientcowboy <holysignals@protonmail.com>
2026-01-21 21:56:14 -0800

    Initalized and applied strings, configured outputs with interpolation and concatenation

--- Program.cs
+++ Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 
-namespace Comp003A
+namespace COMP003A.LectureActivity2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Enter a number: ");
+            String? input1 = Console.ReadLine()!;
+            Console.Write("Enter second number: ");
+            String? input2 = Console.ReadLine()!;
+            double a = double.Parse(input1);
+            double b = double.Parse(input2);
+            double sum = a + b;
+            double average = (a + b) / 2.0;
+            Console.WriteLine("\tUsing Concatenation");
+            Console.WriteLine("Sum = " + sum);
+            Console.WriteLine("Average = " + average);
+            Console.WriteLine("\t Using Interpolation");
+            Console.WriteLine($"Sum = {sum}");
+            Console.WriteLine($"Average = {average}");
         }
     }
 }
